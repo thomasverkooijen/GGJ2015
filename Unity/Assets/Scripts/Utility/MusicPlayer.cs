aa0b82139ef1ee72a91bb95fb18faa31d1835275
thomasverkooijen <info@thomasverkooijen.nl>
2015-01-23 11:11:48 +0100

    Audio preparations.
    Added MusicPlayer and SoundEffectsPlayer.

---
+++ Unity/Assets/Scripts/Utility/MusicPlayer.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MusicPlayer : Singleton<MusicPlayer>
+{
+
+    public enum MusicPlayerMode { Loop, Shuffle };
+    public MusicPlayerMode PlayMode;
+
+    public List<AudioClip> TrackList;
+
+    private AudioSource MusicSourceOne;
+    private AudioSource MusicSourceTwo;
+    private AudioSource ActiveMusicSource;
+    private AudioSource InactiveMusicSource
+    {
+        get
+        {
+            if (ActiveMusicSource == MusicSourceOne)
+            { return MusicSourceTwo; }
+            else { return MusicSourceOne; }
+        }
+    }
+
+    void Start()
+    {
+        MusicSourceOne = gameObject.AddComponent<AudioSource>();
+        MusicSourceTwo = gameObject.AddComponent<AudioSource>();
+        PlayMode = MusicPlayerMode.Loop;
+        ActiveMusicSource = MusicSourceOne;
+        Play(TrackList[0]);
+        StartCoroutine(TestCrossFade());
+    }
+
+    IEnumerator TestCrossFade()
+    {
+        yield return new WaitForSeconds(2.0f);
+        CrossFade(TrackList[1], 1.0f);
+    }
+
+    AudioSource SetupAudioSource()
+    {
+        AudioSource newAudioSource = gameObject.AddComponent<AudioSource>();
+        newAudioSource.loop = true;
+        return newAudioSource;
+    }
+
+    public void Add(AudioClip clip)
+    {
+        TrackList.Add(clip);
+    }
+
+    public void AddAndPlay(AudioClip clip)
+    {
+        TrackList.Add(clip);
+        ActiveMusicSource.clip = clip;
+        ActiveMusicSource.Play();
+    }
+
+    public void Play(AudioClip clip)
+    {
+        ActiveMusicSource.clip = clip;
+        ActiveMusicSource.Play();
+    }
+
+    public void CrossFade(AudioClip clip, float crossfadeDuration)
+    {
+        InactiveMusicSource.clip = clip;
+        InactiveMusicSource.volume = 0.0f;
+        InactiveMusicSource.Play();
+        StartCoroutine(CrossFader(crossfadeDuration));
+    }
+
+    IEnumerator CrossFader(float crossFadeDuration)
+    {
+        while(ActiveMusicSource.volume>0.0f)
+        {
+            ActiveMusicSource.volume -= 0.01f;
+            InactiveMusicSource.volume += 0.1f;
+            yield return new WaitForSeconds(crossFadeDuration/60.0f);
+        }
+        ActiveMusicSource = InactiveMusicSource;
+    }
+}
---
+++ Unity/Assets/Scripts/Utility/SoundEffectPlayer.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SoundEffectPlayer : Singleton<SoundEffectPlayer> {
+
+    public void Play(AudioClip clip)
+    {
+        Play(clip, Vector3.zero, 1.0f, 1.0f, 0.0f);
+    }
+
+    public void Play(AudioClip clip, Vector3 sourcePosition)
+    {
+        Play(clip, sourcePosition, 1.0f, 1.0f, 0.0f);
+    }
+
+    public void Play(AudioClip clip, Vector3 sourcePosition, float pitch)
+    {
+        Play(clip, sourcePosition, pitch, 1.0f, 0.0f);
+    }
+
+    public void Play(AudioClip clip, Vector3 sourcePosition, float pitch, float volume)
+    {
+        Play(clip, sourcePosition, pitch, volume, 0.0f);
+    }
+
+	public void Play(AudioClip clip, Vector3 sourcePosition, float pitch, float volume, float delay)
+    {
+        GameObject newAudioSourceObject = new GameObject("AudioClip_" + clip.name);
+        newAudioSourceObject.transform.parent = transform;
+        newAudioSourceObject.transform.position = sourcePosition;
+        AudioSource newAudioSource = newAudioSourceObject.AddComponent<AudioSource>();
+        newAudioSource.pitch = pitch;
+        newAudioSource.volume = volume;
+        newAudioSource.PlayDelayed(delay);
+        Destroy(newAudioSource, clip.length);
+    }
+}

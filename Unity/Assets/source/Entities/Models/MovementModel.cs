093b3e4890a6b8ed3ca72c48f3b7d1a420ddd0e6
thomasverkooijen <info@thomasverkooijen.nl>
2015-01-24 11:50:16 +0100

    Ididawhoopsie
    Fixed spelling error.

--- Unity/Assets/source/Entities/Models/MovementModel.cs
+++ Unity/Assets/source/Entities/Models/MovementModel.cs
@@ -3,7 +3,7 @@ using System.Collections;
 
 public class MovementModel : MonoBehaviour, IModel
 {
-    public float JumpSpeed { get {return GameSettings.Instance.JumpSpeedBase } set; }
+    public float JumpSpeed { get { return GameSettings.Instance.JumpSpeedBase; } set { this.JumpSpeed = value; } }
     public float AirControl = 0.75f;
     public float HorizontalDrag = 0.9f;
 }

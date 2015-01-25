aaf95b47a6b44232f2a371eff45e7efcdc85f5e5
thomasverkooijen <info@thomasverkooijen.nl>
2015-01-25 04:32:03 +0100

    Added AI input and extremely basic state machine

--- Unity/Assets/source/Entities/Characters/AI/AIPlayerController.cs
+++ Unity/Assets/source/Entities/Characters/AI/AIPlayerController.cs
@@ -1,15 +1,79 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Assets.source.Entities.Characters.AI;
 
 public class AIPlayerController : MovementController {
 
+    private AIState AIState = AIState.Idle;
+    private float AIDecisionDelay = 0.25f;
 	protected AIPlayerView _view;
 	
 	protected override void Awake(){
 		base.Awake();
 		_model 	= gameObject.GetComponent<AIplayer>() as AIplayer;
 		_view	= gameObject.GetComponent<AIPlayerView>();
+        StartCoroutine(ChangeState());
 	}
+
+    IEnumerator ChangeState()
+    {
+        float _decisionValue = Random.value;
+        if (_decisionValue <= 0.2f)
+        {
+            AIState = AIState.Idle;
+        }
+        if (_decisionValue > 0.2f && _decisionValue <= 0.4f)
+        {
+            AIState = AIState.MoveLeft;
+        }
+        if (_decisionValue > 0.4f)
+        {
+            AIState = AIState.MoveRight;
+        }
+        _decisionValue = Random.value;
+        if(_decisionValue>0.75f)
+        {
+            Jump();
+        }
+        yield return new WaitForSeconds(AIDecisionDelay);
+        StartCoroutine(ChangeState());
+    }
+
+    private void HandleAI()
+    {
+        switch(AIState)
+        {
+            case AIState.Idle:
+                Idle();
+                break;
+            case AIState.MoveLeft:
+                MoveLeft(-1.0f);
+                break;
+            case AIState.MoveRight:
+                MoveRight(1.0f);
+                break;
+        }
+    }
+
+    private void Idle()
+    {
+        _xVelocity = MathHelper.IncrementTowards(_xVelocity, 0.0f, _acceleration);
+    }
+
+    private void MoveRight(float p_speed)
+    {
+        _xVelocity = MathHelper.IncrementTowards(_xVelocity, p_speed * _maxSpeed, _acceleration);
+    }
+
+    private void MoveLeft(float p_speed)
+    {
+        _xVelocity = MathHelper.IncrementTowards(_xVelocity, p_speed * _maxSpeed, _acceleration);
+    }
+
+    private void AIJump()
+    {
+        Jump();
+    }
 
 	protected override void Start (){
 		base.Start ();
@@ -19,6 +83,7 @@ public class AIPlayerController : MovementController {
 	
 	protected override void Update(){
 		base.Update();
+        HandleAI();
 		HandleHorizontalMovement();
 		HandleVerticalMovement();
 		HandleGravity();

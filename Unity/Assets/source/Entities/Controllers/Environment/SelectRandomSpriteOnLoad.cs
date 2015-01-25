e9b2d77170a2c0f8980b35dbb7f0ab28fafd9d7d
thomasverkooijen <info@thomasverkooijen.nl>
2015-01-25 12:15:44 +0100

    Level graphics randomization

---
+++ Unity/Assets/source/Entities/Controllers/Environment/SelectRandomSpriteOnLoad.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SelectRandomSpriteOnLoad : MonoBehaviour {
+
+    public Sprite[] AvailableSprites;
+
+	// Use this for initialization
+	void Start () {
+        SpriteRenderer spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = AvailableSprites[Random.Range(0, AvailableSprites.Length)];
+	}
+}
